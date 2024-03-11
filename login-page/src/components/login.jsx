490912a9bda7b07d893a9ab7c8a6e602b16bcb9e
angeiradu <a.iradukund2@alustudent.com>
2024-03-11 14:29:15 -0700

    login page

---
+++ login-page/src/components/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaCircleHalfStroke } from "react-icons/fa6";
+
+const login = () => {
+  return (
+    <div>
+        <div className='px-40 py-20 flex gap-3'>
+          <div className='text-[#80d2d5]'>
+          <FaCircleHalfStroke />
+          </div>
+          <h6 className='text-[#2c5758]'>Ontime</h6>
+        </div>
+    </div>
+  )
+}
+
+export default login
