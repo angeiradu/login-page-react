65ecdb9752914ccfda34bc983e2e07ae99a1ff02
angeiradu <a.iradukund2@alustudent.com>
2024-03-15 08:47:18 -0700

    last commit

--- login-page/src/components/login.jsx
+++ login-page/src/components/login.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { FaCircleHalfStroke } from "react-icons/fa6";
+import Image from '../assets/image.png'
 
 const login = () => {
   return (
-    <div>
-        <div className='px-40 py-20 flex gap-3'>
-          <div className='text-[#80d2d5]'>
-          <FaCircleHalfStroke />
+    <div className='section'>
+      <div className='sub-section'>
+        <div className='grid grid-cols-2' >
+          <div className='bg-[#f8fdfc] col1'>
+            <div className='flex gap-4 px-20 text-[#275556] text-3xl font-bold'>
+              <div className='text-[#00a6ab] text-2xl'>
+                <FaCircleHalfStroke />
+              </div>
+              <h2  className='h1'>Ontime.</h2>
+            </div>
+            <div>
+              <p className='py-11 text-[#275556] text-4xl font-bold p1'>Unlock Your</p>
+              <h1 className='py-11 text-[#275556] text-4xl font-bold p2' >Team <span className='text-[#00a6ab]'>Performance</span></h1>
+              <img src={Image} alt="/"/>
+            </div>
           </div>
-          <h6 className='text-[#2c5758]'>Ontime</h6>
+          <div>
+            <p className='text-[#275556] text-2xl font-bold'>Welcome to Online</p>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
