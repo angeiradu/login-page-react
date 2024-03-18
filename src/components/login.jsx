712d3fede896d7abdf3129e9c8a4b8b32ad1a1d2
angeiradu <a.iradukund2@alustudent.com>
2024-03-18 03:33:49 -0700

    login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,33 +4,33 @@ import Image from '../assets/image.png'
 
 const login = () => {
   return (
-    <div className='section'>
-      <div className='sub-section'>
+    <div className='bg-[#d8edee] text-center py-24 flex '>
+      <div className='bg-[#ffffff] rounded-[60px]  w-full ' >
         <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 gap-2 ' >
-          <div className='bg-[#f8fdfc] col1'>
-            <div className='flex gap-4 px-2 xl:px-20 lg:px-20  text-[#275556] text-3xl font-bold'>
-              <div className='text-[#00a6ab] h0 text-2xl'>
+          <div className='bg-[#f8fdfc] height-[500px] rounded-l-[60px] '>
+            <div className='flex gap-4 px-2 xl:px-20 lg:px-20  text-[#275556] text-3xl font-bold ml-[69px] pt-[25px]'>
+              <div className='text-[#00a6ab] mt-[28px] text-2xl'>
                 <FaCircleHalfStroke />
               </div>
-              <h2  className='h1'>Ontime.</h2>
+              <h2  className='mt-[20px]'>Ontime.</h2>
             </div>
             <div>
-              <p className='py-11 text-[#275556] text-4xl font-bold p1'>Unlock Your</p>
-              <h1 className='py-11 text-[#275556] text-4xl font-bold p2' >Team <span className='text-[#00a6ab]'>Performance</span></h1>
-              <img className='img object-cover' src={Image} alt="/"/>
+              <p className='py-11 text-[#275556] text-4xl font-bold ml-[-137px]'>Unlock Your</p>
+              <h1 className='py-11 text-[#275556] text-4xl font-bold ml-[-28px] mt-[-85px] ' >Team <span className='text-[#00a6ab]'>Performance</span></h1>
+              <img className='mt-[-62px] h-[630px] object-cover' src={Image} alt="/"/>
             </div>
           </div>
-          <div className='col2'>
-            <p className='text-[#275556] text-2xl font-bold p3'>Welcome to Online</p>
-            <p className='text-[#a0b8b8] py-2  font-bold'>Unlock your team performance</p>
-            <p className='text-[#a0b8b8] py-6 p4'>Email address</p>
-            <p className='border p-3 w-80 p5 text-[#275556] font-bold'>a.iradukund2@alustudent.com</p>
-            <p className='text-[#a0b8b8] py-6 p6'>Password</p>
-            <p className='border p-3 w-80 p8 text-[#a0b8b8] '>Enter password</p>
-            <p className='font-bold text-[#275556] p7'>Forgot password?</p>
-            <p className='border p-3 w-80 p9 text-[#ffff] bg-[#00a6ab] font-bold'>Login</p>
-            <p className='p10 text-[#275556] font-bold'>Don't have an account? <span className='text-[#00a6ab] '>Register</span></p>
-            <p className='text-[#a0b8b8] p11 font-bold'>©2021 all rights reserved</p>
+          <div>
+            <p className='text-[#275556] text-2xl font-bold pt-[100px] ml-[-140px]'>Welcome to Online</p>
+            <p className='text-[#a0b8b8] py-2  font-bold  ml-[-140px]'>Unlock your team performance</p>
+            <p className='text-[#a0b8b8] py-6 ml-[-265px]'>Email address</p>
+            <p className='border  border-[#00a6ab] rounded-md p-3 w-80 ml-[142px] text-left  text-[#275556] font-bold'>a.iradukund2@alustudent.com</p>
+            <p className='text-[#a0b8b8] py-6 ml-[-290px]'>Password</p>
+            <p className='border p-3 w-80 ml-[142px] text-left rounded-[5px] text-[#a0b8b8] '>Enter password</p> 
+            <p className='font-bold text-[#275556] ml-[150px] pt-[20px]'>Forgot password?</p>
+            <p className='border p-3 w-80 mt-[20px] ml-[142px] text-center rounded-[5px] text-[#ffff] bg-[#00a6ab] font-bold'>Login</p>
+            <p className='  text-[#275556] font-bold mr-[40px]'>Don't have an account? <span className='text-[#00a6ab] '>Register</span></p>
+            <p className='text-[#a0b8b8]  mt-[120px] font-bold mr-[40px]'>©2021 all rights reserved</p>
           </div>
         </div>
       </div>
