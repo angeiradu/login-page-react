e71f4a4207105de3d9e30eb636acc91520baafdb
angeiradu <a.iradukund2@alustudent.com>
2024-04-10 06:08:31 -0700

    last cimmit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,8 +4,8 @@ import Image from '../assets/image.png'
 
 const login = () => {
   return (
-    <div className='bg-[#d8edee] text-center p-10'>
-      <div className='bg-[#ffffff] rounded-[60px] '>
+    <div className='bg-[#d8edee] text-center p-2 md:p-10 lg:p-10 lx:p-10'>
+      <div className='bg-[#ffffff] lg:rounded-[60px] xl:rounded-[60px] '>
         <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 gap-2  ' >
           <div className='bg-[#f8fdfc] height-[500px] rounded-l-[60px] '>
             <div className='flex gap-4 px-2 xl:px-20 lg:px-20  text-[#275556] text-3xl font-bold ml-[69px] pt-[25px]'>
@@ -20,16 +20,16 @@ const login = () => {
               <img className='mt-[-62px] h-[630px] object-cover ' src={Image} alt="/"/>
             </div>
           </div>
-          <div >
-            <p className='text-[#275556] text-2xl font-bold pt-[100px]  '>Welcome to Online</p>
-            <p className='text-[#a0b8b8] py-2  font-bold  '>Unlock your team performance</p>
-            <p className='text-[#a0b8b8] py-6 px-5 text-left lg:px-[155px] md:px-[182px]'>Email address</p>
-            <input type='text' name='eamil' placeholder='Enter email address'  className=' w-[250px] border  border-[#00a6ab] rounded-md p-3 md:w-80  text-left  text-[#275556] font-bold'/>
-            <p className='text-[#a0b8b8] py-6  px-5 text-left lg:px-[155px] md:px-[182px]'>Password</p>
-            <input type='text' name='password' placeholder='Enter password' className=' w-[250px] border p-3 md:w-80  text-left rounded-[5px] text-[#a0b8b8] ' /> 
-            <p className=' pr-5 font-bold text-[#275556]  pt-[20px]  text-right lg:pr-[155px] md:pr-[182px]'>Forgot password?</p>
-            <button type='button' className=' w-[250px] border p-3 md:w-80 mt-[20px]  text-center rounded-[5px] text-[#ffff] bg-[#00a6ab] font-bold'>Login</button>
-            <p className='  text-[#275556] font-bold '>Don't have an account? <span className='text-[#00a6ab] '>Register</span></p>
+          <div className='px-[10px] md:px-[50px] lg:px-[100px] xl:px-[100px]'>
+            <p className='text-[#275556] text-2xl font-bold pt-[100px] text-left  '>Welcome to Online</p>
+            <p className='text-[#a0b8b8] py-2  font-bold text-left '>Unlock your team performance</p>
+            <p className='text-[#a0b8b8] py-6 text-left '>Email address</p>
+            <input type='text' name='eamil' placeholder='Enter email address'  className=' w-full border  border-[#00a6ab] rounded-md p-3  text-left  text-[#275556] font-bold'/>
+            <p className='text-[#a0b8b8] py-6   text-left'>Password</p>
+            <input type='text' name='password' placeholder='Enter password' className=' w-full border p-3   text-left rounded-[5px] text-[#a0b8b8] ' /> 
+            <p className=' pr-5 font-bold text-[#275556]  pt-[20px]  text-right'>Forgot password?</p>
+            <button type='button' className=' w-full border p-3  mt-[20px]  text-center rounded-[5px] text-[#ffff] bg-[#00a6ab] font-bold'>Login</button>
+            <p className='  text-[#275556] font-bold pt-8'>Don't have an account? <span className='text-[#00a6ab] '>Register</span></p>
             <p className='text-[#a0b8b8]  mt-[120px] font-bold '>©2021 all rights reserved</p>
           </div>
         </div>
